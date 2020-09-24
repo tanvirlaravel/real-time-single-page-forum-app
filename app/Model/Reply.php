414dcfd86ea50125bf8c45c77c90de6bca3deef3
Tanvir <tanvirlaravel@gmail>
2020-09-24 22:26:43 +0600

    Relationships

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,9 +2,22 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function like()
+    {
+        return $this->hasMany(Likes::class);
+    }
 }
