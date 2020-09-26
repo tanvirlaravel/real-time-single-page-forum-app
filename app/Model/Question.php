d2baa779660abc7e138b91a26e2a897f77a7bba2
Tanvir <tanvirlaravel@gmail>
2020-09-26 14:25:00 +0600

    CRUD for Question

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -9,6 +9,11 @@ class Question extends Model
 {
      protected $fillable = ['title','slug','body','user_id','category_id'];
 
+     public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -22,5 +27,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
